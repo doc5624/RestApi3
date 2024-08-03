503321852c8682d5bc2233917e1599a6be830e2d
doc5624 <doc5624@yandex.ru>
2024-08-04 02:18:07 +0700

    задача 3 fail

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSendData() {
+        int text = 5;
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+    }
+}
